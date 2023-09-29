1bede9486aab4f7e9de1b5a9f684b4635514dd06
MahdiBM <github@mahdibm.com>
2023-09-29 20:20:04 +0330

    add install .sh

---
+++ install/swift-format-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# install Nick Lockwood's SwiftFormat
+
+set -o errexit
+
+has_command () {
+    command -v "$1" > /dev/null
+}
+
+if ! has_command "swiftformat" ; then
+    echo "-------- swiftformat already installed. Will skip re-installation."
+    exit 0
+fi
+
+if has_command "curl" ; then
+    echo "-------- curl already installed. That's nice!"
+    exit 0
+elif
+    echo "-------- Will install curl"
+    apt update -y
+    apt install curl -y
+    echo "-------- curl installation successful"
+fi
+
+curl --retry-connrefused https://github.com/MahdiBM/swift-format-repo/raw/main/executables/swiftformat-0.54.2-linux-arm64.deb -o swiftformat-0.54.2.deb
+dpkg -i ./swiftformat-0.54.2.deb
+rm ./swiftformat-0.54.2.deb
