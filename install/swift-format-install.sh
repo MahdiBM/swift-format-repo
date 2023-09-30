7a58aa867c8828a9fbb5d17ff33e3c1db995fcb9
MahdiBM <github@mahdibm.com>
2023-09-30 12:21:20 +0330

    change how it works

--- install/swift-format-install.sh
+++ install/swift-format-install.sh
@@ -13,23 +13,8 @@ print () {
     echo $1
 }
 
-if has_command "swiftformat" ; then
-    print "swiftformat already installed. Will skip re-installation."
-    exit 0
-fi
-
-if has_command "wget" ; then
-    print "wget already installed. That's nice!"
-else
-    print "Will install wget"
-    apt update -y
-    apt install wget -y
-    print "wget installation successful"
-fi
-
 wget --retry-connrefused -O swiftformat-0.54.2.deb https://github.com/MahdiBM/swift-format-repo/raw/main/executables/swiftformat-0.54.2-linux-arm64.deb
-dpkg -i swiftformat-0.54.2.deb
-rm ./swiftformat-0.54.2.deb
+dpkg -i ~/swift-format-repo/executables/swiftformat-0.54.2-linux-arm64.deb
 
 if has_command "swiftformat" ; then
     print "swiftformat installation successful"
