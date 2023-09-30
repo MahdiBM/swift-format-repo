e2d83abcb4b4ccb3ecece85f4e32c2770c54d74c
MahdiBM <github@mahdibm.com>
2023-09-30 12:22:00 +0330

    remove left-over

--- install/swift-format-install.sh
+++ install/swift-format-install.sh
@@ -13,7 +13,6 @@ print () {
     echo $1
 }
 
-wget --retry-connrefused -O swiftformat-0.54.2.deb https://github.com/MahdiBM/swift-format-repo/raw/main/executables/swiftformat-0.54.2-linux-arm64.deb
 dpkg -i ~/swift-format-repo/executables/swiftformat-0.54.2-linux-arm64.deb
 
 if has_command "swiftformat" ; then
